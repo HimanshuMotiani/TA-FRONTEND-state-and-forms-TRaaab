e841ba7e1f9b21134d9795a44b5c69438de1609d
HimanshuMotiani <himanshu.motiani93@gmail.com>
2022-01-07 17:05:31 +0530

    accordian show hide

---
+++ block-BRaabi/accordian-app/src/components/App.js
@@ -0,0 +1,44 @@
+import Accordian from './Accordian'
+function App(params) {
+    let questions = [
+        {
+          Q: 'Who is it for?',
+          A:
+            'Recent college graduates, students taking gap year, dropouts, professionals looking to switch careers, anyone learning web development but needs right environment and guidance. No prior programming knowledge required.',
+        },
+        {
+          Q: 'Does AltCampus help me get a job?',
+          A:
+            'Yes. All we can. We help you at every stage, help you build strong portfolio, prepare for interviews, put you in touch with companies for interview.',
+        },
+        {
+          Q: 'How does the entire process work?',
+          A: 'You can read about it here - How it works.',
+        },
+        {
+          Q: 'Is this course self-paced or batched?',
+          A:
+            'This program follows a blended approach. Students can move at their own pace, however they will be matched with peers learning same concepts.',
+        },
+      ];
+    return (
+        <>
+            <main>
+                <section className="container">
+                    <div>
+                        <h2>Closed State</h2>
+                        <div className="cont">
+                            {
+                                questions.map((item,index)=>(
+                                  <Accordian key={item.Q} question={item} index={index}></Accordian>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    </section>
+            </main>  
+        </>
+    )
+}
+
+export default App;
